b23aca0e353c33b46bec238f3a292cf8ff6580cd
Matt Gibbs <mgibbs@slac.stanford.edu>
2022-06-27 12:00:36 -0700

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='wmctrl',
-    version='0.1.1',
+    version='0.2.0',
     description='A module for interacting with the window manager via the wmctrl tool.',
     long_description=long_description,
 
